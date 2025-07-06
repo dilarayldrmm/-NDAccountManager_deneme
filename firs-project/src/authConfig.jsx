a4cfe96d5e9acdab224d80c9d61b36e30aa2c61e
dilarayldrmm <dilarayldrmm2004@gmail.com>
2025-07-06 14:47:19 +0300

    dördüncü  commit

--- firs-project/src/authConfig.jsx
+++ firs-project/src/authConfig.jsx
@@ -3,7 +3,7 @@ export const msalConfig = {
   auth: {
     clientId: "3305e490-1edf-4279-8c91-f4f497647338",
     authority: "https://login.microsoftonline.com/552a6af7-31b4-4de6-a7d7-cf20278bde03",
-    redirectUri: "http://localhost:3000",
+    redirectUri: "http://localhost:5173",
   },
 };
 
